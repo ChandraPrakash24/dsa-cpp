14dc51ce3d515a175f4d0b0260ec3601f0b830bc
ChandraPrakash24 <2412prakas@gmail.com>
2023-10-06 16:56:59 +0530

    added dsa questions

---
+++ 15 RECURSION CN/13 count zeros.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int countZerosRec(int input) {
+    if (input == 0) {
+        return 1; // Handling the case when input is 0
+    }
+    if (input < 10) {
+        return 0; // Base case
+    }
+    else if (input % 10 == 0) {
+        return 1 + countZerosRec(input / 10);
+    }
+    return countZerosRec(input / 10);
+}
+
+int main() {
+    int input;
+    cin >> input;
+
+    int result = countZerosRec(input);
+    cout << result << endl;
+
+    return 0;
+}
