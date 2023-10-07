ad4b91b52f47366de9212c4f26a51c35dc8e1dd4
ChandraPrakash24 <2412prakas@gmail.com>
2023-10-07 14:01:11 +0530

    added dsa questions

--- 15 RECURSION CN/13 count zeros.cpp
+++ 15 RECURSION CN/13 count zeros.cpp
@@ -1,86 +1,69 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int countZerosRec(int input) {
-    if (input == 0) {
-        return 1; // Handling the case when input is 0
-    }
-    if (input < 10) {
-        return 0; // Base case
-    }
-    else if (input % 10 == 0) {
-        return 1 + countZerosRec(input / 10);
-    }
-    return countZerosRec(input / 10);
+int cnt=0;
+
+int countZeros(int n){
+    // base cases
+    string numberStr = to_string(n);
+    if(numberStr.length() == 0) return (cout<<numberStr.length()<<"this hit",cnt);
+    
+    // Base case: When n becomes 0, return 1 as it has one zero.
+    if (n == 0 && numberStr.length() == 1 ) return 1;
+
+    // Base case: When n has only one digit and it's not 0, return 0.
+    if (n < 10 && n != 0) return cnt;
+
+    // Extract the last digit.
+    int lastDigit = n % 10;
+    cout<<lastDigit<<endl;
+
+    // Count the number of zeros in the remaining digits.
+    int restCount = countZeros(n / 10);
+
+    // Recursive call.
+    if (lastDigit == 0) return restCount + 1;
+    else return restCount;
+
 }
 
-int main() {
-    int input;
-    cin >> input;
+int main()
+{
+    int n; cin>>n;
 
-    int result = countZerosRec(input);
-    cout << result << endl;
-
-    //recurion
-    //recurion
+    cout<<countZeros(n);
+   
     return 0;
 }
 
+// #include<bits/stdc++.h>
+// using namespace std;
 
+// int countZeros(int n, int cnt){
+//     // base cases
+//     string numberStr = to_string(n);
+//     if(numberStr.length() == 1) return (cnt,cnt);
+    
+//     // Base case: When n becomes 0, return 1 as it has one zero.
+//     if (n == 0) return 1;
 
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
-    //recurion
+//     // Base case: When n has only one digit and it's not 0, return 0.
+//     if (n < 10 && n != 0) return 0;
+
+//     // Extract the last digit.
+//     int lastDigit = n % 10;
+
+//     // Recursive call.
+//     if (lastDigit == 0) return countZeros(n / 10, cnt + 1);
+//     else return countZeros(n / 10, cnt);
+
+// }
+
+// int main()
+// {
+//     int n; cin>>n;
+//     int cnt=0;
+//     cout<<countZeros(n,cnt);
+   
+//     return 0;
+// }
