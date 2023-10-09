22f60807a2c2d4f4c5655f9d290d2b2b974b8be2
ChandraPrakash24 <2412prakas@gmail.com>
2023-10-09 10:26:08 +0530

    added dsa questions

--- 15 RECURSION CN/23 Program for length of a string using recursion.cpp
+++ 15 RECURSION CN/23 Program for length of a string using recursion.cpp
@@ -1,20 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int StringLength(string str){
+int stringLength(string str){
     if(str.length() == 0) return 0;
     if(str.length() == 1) return 1;
 
-    if(str.front() == ' ') return StringLength(str.substr(1));
+    if(str.front() == ' ') return stringLength(str.substr(1));
 
-    return 1 + StringLength(str.substr(1));
+    return 1 + stringLength(str.substr(1));
 
 }
 
 int main()
 {
     string s = "lord of the rings"; 
-    cout<<StringLength(s);
+    cout<<stringLength(s);
     
     return 0;
 }
