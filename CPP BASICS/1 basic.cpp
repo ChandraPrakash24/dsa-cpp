4e5e3f490415b1bb76794fa4a109981397ba12e2
ChandraPrakash24 <2412prakas@gmail.com>
2024-03-13 19:33:52 +0530

    added dsa questions

---
+++ CPP BASICS/1 basic.cpp
@@ -0,0 +1,69 @@
+/* why cpp
+- efficent and spawnlp
+- most popular
+- poweful liberary STL
+- cpp > java > python > js
+
+
+Q. why cpp is fast?
+- its is a compilation language (copmile time is not counted ony run time is sounted) whwre is suppose in js it is interprited so it is counted as rountime
+
+CONSTAINTS
+
+Integer constants: 4 , 62, -90
+Decimal constants: 4.2 , 3.14, 0.33390, -2.2
+Character constants: 'f', '5', '~', '\n' 
+String literals: "Hello :D", "MyP@ssword123!", "a", "1"
+
+
+endl or '\n'
+'\' backslash is a escape character (escape this (during parshing) and consider next char)
+
+
+ARETHEMATIC OPERATOR
+
+( cpp follow bodmas rule)
+
+=,-,*,/,% 
+
+ex:-
+10 / 3 = 3
+10.3 / 3 = 3.3333
+10 / 3.0 = 3.3333
+10.3 / 3.0 = 3.3333
+
+14 % 3 = 2
+14.0 % 3 = 2 [err] // expression must have integral or unscoped enum type
+(double)14 % 3 [err]
+
+
+/ quitent
+5  reminder
+
+cout<<'f'; // f
+cout<<(int)'f'; // 102
+cout<<'f'+1; // 103
+cout<<(char)('f'); // g
+cout<<(char)('f' + 1); // g
+
+
+cout<<'dhlks'; //6251387 (garabge value or UD or un defined or un valid~UV)
+
+
+NOTE: character --> int
+      int --x char
+
+
+*/
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+     
+
+
+    return 0;
+}
