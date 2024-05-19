0032e8e6fc1b4caecc4a47bbf466b6b1bdda541b
ChandraPrakash24 <2412prakas@gmail.com>
2024-05-19 17:05:37 +0530

    added dsa questions

--- CPP BASICS/1 basic.cpp
+++ CPP BASICS/1 basic.cpp
@@ -5,7 +5,22 @@ int c=55;
 
 int main()
 {
-    int c=4;
-    cout<<c<<" "<<::c<<endl;
+    int a = 3;
+    int * b = &a;
+
+    // cout<<a<<endl; 
+    // cout<<&a<<endl;
+    cout<<b<<endl;
+    // cout<<&b<<endl;
+    // cout<<*b<<endl;
+
+    int ** c = &b;
+    // cout<<c<<endl;
+    // cout<<*c<<endl;
+    cout<<**c<<endl;
+
+    // c --> b --> a
+
     return 0;
+
 }
