103a6f7e979c8e60acf25401567a9a2bcc66cdb9
ChandraPrakash24 <2412prakas@gmail.com>
2024-05-20 14:01:15 +0530

    added dsa questions

--- CPP BASICS/1 basic.cpp
+++ CPP BASICS/1 basic.cpp
@@ -1,33 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+typedef struct employee
+{
+    int id;
+    float salary;
+    char gender;
+    string name;
+} ep;
+
+
 int main()
 {
-    int myArr[6] = {1,2,3,4,5,6}; 
+     
+    ep jhon;
+    jhon.gender = 'm';
+    jhon.id = 4;
+    jhon.name = "jhone doe";
+    jhon.salary = 120.001;
 
-    int * p = myArr;
-    cout<<p<<endl;
-    cout<<&p<<endl;
-    cout<<myArr<<endl;
-    // cout<<myArr[myArr]<<endl;
-    cout<<&myArr<<endl;
+    // cout<<jhon.gender<<endl;
+    // cout<<jhon.id<<endl;
+    // cout<<jhon.name<<endl;
+    // cout<<jhon.salary<<endl;
 
-    cout<<*p++<<endl;
-    cout<<*p++<<endl;
-    cout<<*p++<<endl;
-    cout<<*p++<<endl;
-    cout<<*p++<<endl;
-    cout<<*p++<<endl;
-    cout<<*p++<<endl;
-    cout<<*p++<<endl;
-    cout<<*p++<<endl;
-    cout<<*p++<<endl;
-
-    for(int i=0;i<6; i++){
-        // cout<<p[i]<<endl;
-        // cout<<*(p+i)<<endl;
-            // cout<<i<<endl;
-    }
+    // const string ci = "z";
+    cout<<typeid(jhon).name();
+// std::cout << typeid(ci).name() << '\n';
 
 
     return 0;
