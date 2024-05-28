6a5d4db3fe222596b55f45391cbf0cb16eb50d82
ChandraPrakash24 <2412prakas@gmail.com>
2024-05-28 09:27:09 +0530

    added dsa questions

--- CPP BASICS/1 basic.cpp
+++ CPP BASICS/1 basic.cpp
@@ -1,40 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Pointer{
-    private:
-        int x,y;
-    public:
-        Pointer(int a, int b){
-            x=a;
-            y=b;
-        }
-
-        friend void distance(Pointer, Pointer);
-
-        void display(void);
-};
-
-void Pointer :: display(void){
-    // cout<<"Points are : "<<x<<"and "<<y<<"and there distance are:"<<distance(x.b, y.a)<<endl;
-    cout<<"Points are : "<<x<<"and "<<y<<endl;
-}
-
-void distance(Pointer p1, Pointer p2){
-    cout<<"Distance is"<<p1.y-p1.x<<endl;
-    cout<<"Distance is"<<p2.y-p2.x<<endl;
-}
-
 int main()
 {
-     
-    Pointer p1(3,2), p2(6,3);
+    int n,cnt=0; cin>>n;
 
-    p1.display();
-    p2.display();
+    if (n == 0) {
+        cout<<"only one digit that is : "<<n<<endl;
+        return 0;
+    }
+    while(n != 0){
+        cout<<"Last digit is : "<<n%10<<endl;
+        cnt++;
+        n /= 10;
+    } 
+    
+    cout<<"No of digit Count is "<<cnt<<endl;
 
-    distance(p1,p2);
-
+    // cout<<n;
 
     return 0;
 }
