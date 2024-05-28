ea3088cd547c6afe7e3dc1b683e91df26780d4dd
ChandraPrakash24 <2412prakas@gmail.com>
2024-05-28 16:04:25 +0530

    added dsa questions

--- CPP BASICS/1 basic.cpp
+++ CPP BASICS/1 basic.cpp
@@ -1,4 +1,4 @@
-// is any set bit in a number and what is its position
+// is only one set bit in a number and what is its position
 
 #include <iostream>
 using namespace std;
@@ -17,11 +17,13 @@ int main() {
             n = n >>1;
         }else if((n & 1)==1){
             count++;
+            n = n >>1;
             break;
         }
     }
 
-    cout<<"Set bit at position : "<<count<<endl;
+    if(!n) cout<<"Set bit at position : "<<count<<endl;
+    else cout<<"This no contain multiple set bit"<<endl;
 
   return 0;
 }
