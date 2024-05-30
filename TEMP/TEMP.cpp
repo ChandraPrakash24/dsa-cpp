f20969fd9b42bc6a319fb75c0399b8fc713a332a
ChandraPrakash24 <2412prakas@gmail.com>
2024-05-30 17:03:50 +0530

    added dsa questions

--- TEMP/TEMP.cpp
+++ TEMP/TEMP.cpp
@@ -1,27 +1,22 @@
-// Roap Cutting (recursion)
+// All subset of string (recursion)
 
 #include <iostream>
 using namespace std;
 
-int maxPieces(int n, int a, int b, int c){
-    if(n == 0) return 0;
-    if(n < 0) return -1;
+void stringSubSets(string s, string current = "", int inx = 0){
 
-    int temp1 = maxPieces(n-a,a,b,c);
-    int temp2 = maxPieces(n-b,a,b,c);
-    int temp3 = maxPieces(n-c,a,b,c);
+    if(inx == s.length()){
+        cout<<current<<endl;
+        return;
+    }
 
-    int pieces = max(max(temp1,temp2),temp3);
-
-    if(pieces == -1) return -1;
-
-    return pieces+1;
+    stringSubSets(s,current,inx+1);
+    stringSubSets(s,current+s[inx],inx+1);
 
 }
 
 int main()
 {
-    // cout<<maxPieces(15,5,8,7); // 3
-    cout<<maxPieces(23,12,14,11); // 2
+    stringSubSets("A");
     return 0;
 }
