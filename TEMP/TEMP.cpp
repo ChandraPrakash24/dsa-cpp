914b1bb68020639fda4d6e0920f0acf572c37d2a
ChandraPrakash24 <2412prakas@gmail.com>
2024-06-04 11:53:23 +0530

    added dsa questions

--- TEMP/TEMP.cpp
+++ TEMP/TEMP.cpp
@@ -1,63 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void insert(stack<int> &s,int top){
-    if(s.empty()){
-        s.push(top);
+void printSubSet(string &str, string current = "", int index = 0){
+    if(index == str.length()){
+        cout<<current<<endl;
         return;
     }
 
-    int first = s.top();
-    s.pop();
-    insert(s,top);
-    s.push(first);
-
-} 
-
-void reverceStack(stack<int> &s){
-
-    if(s.size() == 1){
-        return;
-    }
-
-    int top = s.top();
-    s.pop();
-    reverceStack(s);
-    insert(s,top);    
+    printSubSet(str, current, index + 1);
+    printSubSet(str, current+str[index], index + 1);
 }
 
 int main()
 {
-    stack<int> s;
+    string s = "abc";
 
-        // Push elements onto the stack
-    s.push(1);
-    s.push(2);
-    s.push(3);
-    s.push(4);
-    s.push(5);
-    s.push(6);
-
-    stack<int> temp = s;
-
-    cout<<"Orignal Stack: "<<endl;
-    while (!temp.empty()) {
-        cout << temp.top() << " ";
-        temp.pop();
-    }
-        cout << endl;
-
-    // Delete middle element
-    reverceStack(s);
-    // deleteMiddleElement(s,3);
-
-    cout << "Stack after deleting middle element: ";
-    while (!s.empty()) {
-        cout << s.top() << " ";
-        s.pop();
-    }
-    cout << endl;
-
+    printSubSet(s); 
 
     return 0;
 }
