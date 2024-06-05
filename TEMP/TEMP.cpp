be985ad676f14b5c8780100f638d95d0d4217269
ChandraPrakash24 <2412prakas@gmail.com>
2024-06-05 17:45:08 +0530

    added dsa questions

--- TEMP/TEMP.cpp
+++ TEMP/TEMP.cpp
@@ -1,49 +1,22 @@
-
-//{ Driver Code Starts
-//Initial Template for C++
-
-
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-
-// } Driver Code Ends
-//User function Template for C++
-
-class Solution{
-  public:
-    //Complete this function
-    int digitalRoot(int n) {
-        if (n < 10)
-            return n; // Base case: return single digit number
-        
-        int sum = 0;
-        while (n > 0) {
-            sum += n % 10;
-            n /= 10;
-        }
-        
-        return digitalRoot(sum); // Recursive call with accumulated sum
-    }
+class Car{
+    public:
+        string color;
+        void race(void);
 };
 
-//{ Driver Code Starts.
-
-
-int main() {
-	int T;
-	cin>>T;//taking testcases
-	while(T--)
-	{
-	    int n;
-	    cin>>n;//taking number n
-	    
-	    //calling digitalRoot() function
-	    Solution obj;
-	    cout<<obj.digitalRoot(n)<<endl;
-	    
-	    
-	}
-	return 0;
+void Car :: race(void){
+    cout<<"Bhroom"<<endl;
 }
-// } Driver Code Ends
+
+int main()
+{
+    Car c1;
+    c1.color="black";
+    cout<<c1.color<<endl;
+    c1.race();
+
+    return 0;
+}
