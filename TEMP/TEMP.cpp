2ffc38f7669319463a7eb6b297d5f8ef9b79884e
ChandraPrakash24 <2412prakas@gmail.com>
2024-06-06 16:03:52 +0530

    added dsa questions

--- TEMP/TEMP.cpp
+++ TEMP/TEMP.cpp
@@ -1,12 +1,10 @@
-// print unique subset
-
 #include<bits/stdc++.h>
 using namespace std;
 
-void printPair(int arr[],int size=5){
+void pairSum(int arr[],int size=5){
     for(int i=0;i<size;i++){
         for(int j=i+1;j<size;j++){
-            cout<<arr[i]<<" , "<<arr[j]<<endl;
+            cout<<arr[i]<<" , "<<arr[j]<<" sum is: "<<arr[i]+arr[j]<<endl;
         }
         cout<<endl;
     }
@@ -16,8 +14,7 @@ int main()
 {
     int arr[] = {1,2,3,4,5}; 
     
-    printPair(arr);
-
+    pairSum(arr);
 
     return 0;
 }
