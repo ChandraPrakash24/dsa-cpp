2d86dc632bb8570ed1d149a7cb7cf9201794a96c
ChandraPrakash24 <2412prakas@gmail.com>
2024-06-14 14:50:52 +0530

    added dsa questions

--- TEMP/TEMP.cpp
+++ TEMP/TEMP.cpp
@@ -1,96 +1,32 @@
-#include <iostream>
-#include <string>
+#include<bits/stdc++.h>
 using namespace std;
 
-void permutationWithSpaces(string str, string out) {
-    // Base case: If the input string is empty, print the output
-    if (str.empty()) {
-        cout << out << endl;
+
+void solve(string s, string out){
+
+    if(s.empty()){
+        cout<<out<<endl;
         return;
     }
 
+    solve(s.substr(1),out + ' ' + s[0]);
+    solve(s.substr(1),out + s[0]);
 
-    // Include a space followed by the first character of the input string in the output
-    permutationWithSpaces(str.substr(1), out + ' ' + str[0]);
-
-    // Include only a space in the output
-    permutationWithSpaces(str.substr(1), out + str[0]);
 }
 
-int main() {
-    string input;
-    cout << "Enter a string: ";
-    cin >> input;
+int main()
+{
+    string s; cin>>s;
 
-    permutationWithSpaces(input.substr(1), string(1, input[0]));
-
+    solve(s.substr(1),string(1,s[0])); 
+    
     return 0;
 }
 
+// out:-
+// abc
 
-// #include<bits/stdc++.h>
-// using namespace std;
-
-// void solve(string i, string o){
-
-//     if(o.length() == 0){
-//         cout<<o<<endl;
-//         return;
-//     }
-
-//     // string op1 = "", op2 = "";
-//     // op1.push_back(' ');
-//     // op1.push_back(i[0]);
-
-//     // op2.push_back(i[0]);
-
-//     string op1 = o + ' ' + i[0];
-//     string op2 = o + i[0];
-
-//     i.erase(i.begin() + 0);
-
-//     solve(i, op1);
-//     solve(i, op2);
-
-//     return;
-
-// }
-
-
-// int main()
-// {
-//     string input; cin >> input;
-
-//     string output = "";
-
-//     output.push_back(input[0]);
-//     input.erase(input.begin() + 0); 
-    
-//     solve(input,output);
-
-
-//     return 0;
-// }
-
-// #include<bits/stdc++.h>
-// using namespace std;
-
-// void permutationWithSpaces(string str, string out, int index){
-
-//     if(index == 3){
-//         cout<<out<<endl;
-//         return;
-//     }
-
-//     return permutationWithSpaces(str.pop_back(), str + " " + str[0], index+1);
-//     return permutationWithSpaces(str.pop_back(), str + str[0], index+1);
-// }
-
-// int main()
-// {
-//     permutationWithSpaces("ABC", "A", 1);    
-    
-
-
-//     return 0;
-// }
+// a b c
+// a bc
+// ab c
+// abc
