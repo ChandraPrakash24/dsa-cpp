04fd19157e363f53f42f292992d2b7592f598baa
ChandraPrakash24 <2412prakas@gmail.com>
2024-06-17 11:49:56 +0530

    added dsa questions

--- TEMP/TEMP.cpp
+++ TEMP/TEMP.cpp
@@ -1,31 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int deleteAt(int arr[], int& current_size, int capacity, int position){
-
-    cout<<"Current capicity is: "<<current_size<<endl;
-    if(position > current_size || position > capacity || position == 0) return -1;
-    
-    for(int i=position-1; i<current_size; i++){
-        arr[i] = arr[i+1];
+int getLargestElIndex(int arr[], int n){
+    int large = 0;
+    for(int i=1;i<n;i++){
+        if(arr[i] > arr[large]) large = i;
     }
 
-    cout<<"Now current capicity is: "<<--current_size<<endl;
-
-    for(int i=0; i<current_size; i++) cout<<arr[i]<<" ";
-    
-    cout<<endl;
-
-    return current_size;
-
+    return large;
 }
 
-int main(){
+int getSecondLargestElIndex(int arr[], int n){
+    int largest = getLargestElIndex(arr,n);
+    int res = -1;
 
-    int arr[5] = {3,8,12,5,6};  
-    int current_size = 5;
-    cout<<deleteAt(arr,current_size,5,1);
-    cout<<deleteAt(arr,current_size,5,4);
+    for(int i=0; i<n; i++){
+        if(arr[i] != arr[largest]){
+            if(res == -1) res = i;
+            else if(arr[i] > arr[res]) res = i;
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    int arr[] = {5,20,12,20,10}; 
+    
+    int sl = getSecondLargestElIndex(arr,5);
+
+    cout<<"The second largest element is : "<<arr[sl]<<" at position : "<<sl+1<<" at array index : (zero based) "<<sl<<endl;
+    // OUT:
+    // The second largest element is : 12 at position : 3 at array index : (zero based) 2
 
     return 0;
 }
