b3c613e455f88592faa09b7be389ef714680ce1e
ChandraPrakash24 <2412prakas@gmail.com>
2024-06-17 11:04:16 +0530

    added dsa questions

--- TEMP/TEMP.cpp
+++ TEMP/TEMP.cpp
@@ -1,23 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int insertElementAt(int arr[], int& current_size, int capacity, int position, int element){
+int deleteAt(int arr[], int& current_size, int capacity, int position){
 
     cout<<"Current capicity is: "<<current_size<<endl;
-    if(current_size == capacity) return -1;
-
-    // int pos = position-1;
-
-    for(int i=current_size-1; i>=position-1; i--){
-        arr[i+1] = arr[i];
+    if(position > current_size || position > capacity || position == 0) return -1;
+    
+    for(int i=position-1; i<current_size; i++){
+        arr[i] = arr[i+1];
     }
 
-    arr[position-1] = element;
-    current_size++;
-    cout<<"Now current capicity ios: "<<current_size<<endl;
+    cout<<"Now current capicity is: "<<--current_size<<endl;
 
     for(int i=0; i<current_size; i++) cout<<arr[i]<<" ";
-
+    
     cout<<endl;
 
     return current_size;
@@ -25,11 +21,11 @@ int insertElementAt(int arr[], int& current_size, int capacity, int position, in
 }
 
 int main(){
-    int arr[5] = {20,5,7};  
-    int current_size = 3;
-    cout<<insertElementAt(arr,current_size,5,2,11);
-    cout<<insertElementAt(arr,current_size,5,5,64);
-    cout<<insertElementAt(arr,current_size,5,3,22);
+
+    int arr[5] = {3,8,12,5,6};  
+    int current_size = 5;
+    cout<<deleteAt(arr,current_size,5,1);
+    cout<<deleteAt(arr,current_size,5,4);
 
     return 0;
 }
