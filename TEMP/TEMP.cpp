08599d886dcaad7ed85ed0a62168c1820edaec4a
ChandraPrakash24 <2412prakas@gmail.com>
2024-06-18 11:50:49 +0530

    added dsa questions

--- TEMP/TEMP.cpp
+++ TEMP/TEMP.cpp
@@ -1,21 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool isSorted(int arr[], int n){
-    for(int i =0; i<n-1; i++){
-        if(arr[i+1] < arr[i]){
-            return false;
-            break;
-        }
+void reverseArr(int *arr, int n){
+    int i=0, j=n-1;
+    while(i<=j){
+        swap(arr[i++],arr[j--]);
     }
-    return true;
 }
 
 int main()
 {
 
-    int arr[] = {10,8,9};
-    cout<<isSorted(arr,3); 
+    int arr[] = {1,8,8,9};
+    reverseArr(arr,4); 
+
+    for(auto& i : arr) cout<<i<<" ";
 
     return 0;
 }
