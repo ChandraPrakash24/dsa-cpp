b1110e56ea3a94bd4b622d83cb84d90755ef9ab6
ChandraPrakash24 <2412prakas@gmail.com>
2024-06-19 12:47:51 +0530

    added dsa questions

--- 15 RECURSION CN/17 replace x by y in a character array.cpp
+++ 15 RECURSION CN/17 replace x by y in a character array.cpp
@@ -1,22 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void replaceXbyY(char s[]){
+void replaceXbyY(char s[]) {
     //base
-    if(s[0] == '\0') return;
+    if (s[0] == '\0') return;
 
     //process
-    if(s[0] == 'b') s[0] = 'x';
+    if (s[0] == 'b') s[0] = 'x';
 
     //call
-    replaceXbyY(s+1);
+    replaceXbyY(s + 1);
 }
 
 int main()
 {
-    char s[100];  cin>>s;
+    char s[100];  cin >> s;
     replaceXbyY(s);
-    cout<<s;
+    cout << s;
 
     return 0;
 }
--- TEMP/TEMP.cpp
+++ TEMP/TEMP.cpp
@@ -1,32 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void remDupInSortedArr(int *arr, int& n){
-    int j=1;
-    for(int i=1;i<n;i++){
-        if(arr[i] != arr[j-1]){
-            arr[j] = arr[i];
-            j++;
+void moveZeros(int *arr, int n){
+    int k=0;
+    for(int i=0;i<n;i++){
+        if(arr[i] != 0){
+            swap(arr[k++],arr[i]);
         }
     }
-    n=j;
 }
 
-
 int main()
-{
+{  
+    // int arr[] = {8,5,0,10,0,20};
+    int arr[] = {10,10,10};
 
-    int arr[] = {1,1,1,8,8,9,9,10,10,10,11};
-    // int arr[] = {1,8,9,10,11};
-    // int arr[] = {10,10,10};
-    // int arr[] = {1,8,9,10,10,10,11};
-    int n=11;
-    remDupInSortedArr(arr,n); 
-
-
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
+    moveZeros(arr,6);
+    
+    for(auto& i : arr) cout<<i<<" ";
 
     return 0;
 }
