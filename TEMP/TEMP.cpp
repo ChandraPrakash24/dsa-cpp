3abdb1971b863d73ca554e74735fc0d2f3b4a25a
ChandraPrakash24 <2412prakas@gmail.com>
2024-06-19 12:52:15 +0530

    added dsa questions

--- TEMP/TEMP.cpp
+++ TEMP/TEMP.cpp
@@ -1,21 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void moveZeros(int *arr, int n){
-    int k=0;
-    for(int i=0;i<n;i++){
-        if(arr[i] != 0){
-            swap(arr[k++],arr[i]);
-        }
+void leftRotateByOne(int * arr, int n){
+    int f = arr[0];
+    for(int i=0;i<n-1;i++){
+        arr[i] = arr[i+1];
     }
+    arr[n-1] = f;
 }
 
 int main()
-{  
-    // int arr[] = {8,5,0,10,0,20};
-    int arr[] = {10,10,10};
+{
+    int arr[] = {1,2,3,4,5}; 
 
-    moveZeros(arr,6);
+    leftRotateByOne(arr,5);
     
     for(auto& i : arr) cout<<i<<" ";
 
