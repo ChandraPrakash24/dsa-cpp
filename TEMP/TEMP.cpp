53ba6e7107d5a72d2d78b5ba5dde3b3258a8ecbd
ChandraPrakash24 <2412prakas@gmail.com>
2024-06-20 11:44:14 +0530

    added dsa questions

--- TEMP/TEMP.cpp
+++ TEMP/TEMP.cpp
@@ -1,19 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void leftRotateByOne(int * arr, int n){
-    int f = arr[0];
-    for(int i=0;i<n-1;i++){
-        arr[i] = arr[i+1];
+void reverse(int * arr, int l, int r){
+    while(l < r){
+        swap(arr[l++],arr[r--]);
     }
-    arr[n-1] = f;
+}
+
+void leftRotateByD(int * arr, int n, int d){
+    reverse(arr,0,d-1);
+    reverse(arr,d,n-1);
+    reverse(arr,0,n-1);
 }
 
 int main()
 {
     int arr[] = {1,2,3,4,5}; 
 
-    leftRotateByOne(arr,5);
+    leftRotateByD(arr,5,2);
     
     for(auto& i : arr) cout<<i<<" ";
 
