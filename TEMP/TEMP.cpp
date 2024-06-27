4a5d9dd037ccbce3b70d0e1304632fea01cf0580
ChandraPrakash24 <2412prakas@gmail.com>
2024-06-27 12:29:17 +0530

    added dsa questions

--- TEMP/TEMP.cpp
+++ TEMP/TEMP.cpp
@@ -1,29 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int bestTimeToBuyAndSell(int * price, int start, int end){
-    if(start >= end) return 0;
+int trappingTheRainWater(int * arr, int n){
+    int res=0;
 
-    int maxProfit=0;
-    for(int i=start; i<end;i++){
-        for(int j=i+1;j<=end;j++){
-            if(price[j] > price[i]){
-                int current_max = price[j] - price[i] + bestTimeToBuyAndSell(price, start, i-1) + bestTimeToBuyAndSell(price, j+1, end);
-                maxProfit = max(maxProfit, current_max);
-            }
+    for(int i=1;i<n-1;i++){
+
+        int lMax=arr[i];
+        for(int l=0;l<i;l++){
+            // if(arr[l] > lMax) lMax = arr[i]; 
+            lMax = max(arr[l],lMax);
         }
+
+        int rMax=arr[i];
+        for(int r=i+1;r<n;r++){
+            // if(arr[r] > rMax) rMax = arr[i]; 
+            rMax = max(arr[r],rMax);
+        }
+
+        res += (min(lMax,rMax) - arr[i]);
     }
-    return maxProfit;
+    return res;
 }
 
 int main()
 {
-    int arr[] = {1,5,3,1,2,8}; // 11
+    // int arr[] = {5,0,6,2,3}; // 6
+    // int arr[] = {3,0,1,2,5}; // 6
+    int arr[] = {4,2,0,3,2,5}; // 9
     int n=6;
-    // int arr[] = {1,5,3,8,12}; // 13
-    // int n=5;
 
-    cout<<bestTimeToBuyAndSell(arr,0,n-1);
+    cout<<trappingTheRainWater(arr,n);
 
     return 0;
 }
