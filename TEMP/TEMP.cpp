da79c2c6d7b9f0303375f925b34a34c13e304e0c
ChandraPrakash24 <2412prakas@gmail.com>
2024-07-04 11:55:43 +0530

    added dsa questions

--- TEMP/TEMP.cpp
+++ TEMP/TEMP.cpp
@@ -1,42 +1,48 @@
-// maximum sub array sum
-// TC: O(n^2), SC: O(1)
+// permutation with case Changes
 
 #include<bits/stdc++.h>
 using namespace std;
 
-int maxSubArraySum(int * arr, int n){
-    int sum=0;
-    // int sum=arr[0];
+void permutationWithCaseChange(string ip_string, string op){
 
-    for(int i=0;i<n;i++){
-        sum+=arr[i];
-        if(sum<0) sum = 0;
+    if(ip_string.empty()){
+        cout<<op<<endl;
+        return;
     }
 
-    // for(int i=0;i<n-2;i++){
-    //     if((sum += arr[i+1]) < 0){
-    //         sum = arr[i+2];
-    //         cout<<"sum:"<<sum<<endl;
-    //         continue;
-    //         cout<<"after continue"<<endl;
-    //     }
-    //     sum+=arr[i+1];
-    // }
+    string op1 = op;
+    string op2 = op;
 
-    return sum != 0 ? sum : -1;
+    op1.push_back(ip_string[0]);
+    op2.push_back(toupper(ip_string[0]));
+
+
+    ip_string.erase(ip_string.begin());
+
+    permutationWithCaseChange(ip_string,op1);
+    permutationWithCaseChange(ip_string,op2);
+
+    return;
+
 }
 
 
 int main()
 {
-    int arr[] = {2,3,-8,7,-1,2,3}; // 11
-    int n=7;
-    // int arr[] = {5,8,3}; // 16
-    // int n=3;
-    // int arr[] = {-6,-1,-8}; // -1
-    // int n=3;
+    string s; cin>>s;
+    string op = "";
 
-    cout<<maxSubArraySum(arr,n);
+    permutationWithCaseChange(s,op); 
     
+    /*
+        i/p:-
+            ab
+        output:-
+            ab
+            aB
+            Ab
+            AB
+     */
+
     return 0;
 }
