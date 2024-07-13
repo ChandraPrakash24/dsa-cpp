52a31ac93449de9a609f2057c1c1b98b3e1bf70e
ChandraPrakash24 <2412prakas@gmail.com>
2024-07-13 12:25:23 +0530

    added dsa questions

--- TEMP/TEMP.cpp
+++ TEMP/TEMP.cpp
@@ -1,47 +1,68 @@
-
 #include<bits/stdc++.h>
 using namespace std;
 
-int solve(vector<int> arr,int index, int k){
-    if(arr.size() == 1) return arr[0];
+// void solve(vector<int> arr, int sum, int curr_sum, int& count) {
+//     if (sum == 0) { count = 1; return; }
+//     if (arr.empty()) {
+//         if (curr_sum == sum) {
+//             count += 1;
+//         } 
+//         return; 
+//     }
 
-    index = (index+k) % arr.size();
+//     int el = arr[0];
+//     int curr_sum1 = curr_sum;
+//     int curr_sum2 = curr_sum;
+//     curr_sum2 = curr_sum2 + el;
+//     arr.erase(arr.begin());
+//     solve(arr, sum, curr_sum1, count);
+//     solve(arr, sum, curr_sum2, count);
+//     return;
+// }
 
-    arr.erase(arr.begin() + index);
 
-    for(auto& i : arr) cout<<i<<" ";
-    cout<<endl;
+int solve(vector<int> arr, int sum, int curr_sum, int count) {
+    if (sum == 0) { return 1; }
+    if (arr.empty()) {
+        if (curr_sum == sum) {
+            count += 1;
+        } 
+        return count; 
+    }
 
-
-    return solve(arr,index, k);
-
+    int el = arr[0];
+    int curr_sum1 = curr_sum;
+    int curr_sum2 = curr_sum;
+    curr_sum2 = curr_sum2 + el;
+    arr.erase(arr.begin());
+    int count1 =  solve(arr, sum, curr_sum1, count);
+    int count2 =  solve(arr, sum, curr_sum2, count);
+    return count1 + count2;
 }
 
-int josephusProblem(int n, int k){
 
-    vector<int> arr(n);
+int subsetSum(vector<int> arr, int sum){
 
-    for(int i=1;i<=n;i++) arr[i-1] = i;
+    int count = 0, curr_sum = 0;
 
-    k--;
+    return solve(arr, sum, curr_sum, count);
 
-    int index=0;
-
-    cout<<"size: "<<arr.size()<<endl;;
-
-    for(auto& i : arr) cout<<i<<endl;
-
-    cout<<"k is : "<<k<<endl;
-    
-    return solve(arr,index,k);
 }
 
 int main()
 {
+    vector<int> arr = {10,5,2,3,6}; // 2
+    int sum = 8;
+    // vector<int> arr = {1,2,3}; // 1
+    // int sum = 4;
+    // vector<int> arr = {10,20,15}; // 0
+    // int sum = 37;
+    // vector<int> arr = {10,20,30}; // 1
+    // int sum = 60;
+    // vector<int> arr = {10,20,30}; // 1 {empty array}
+    // int sum = 0;
 
-    // cout<<josephusProblem(5,2); // 3
-    cout<<josephusProblem(5,3); // 4
+    cout<<subsetSum(arr,sum); 
 
-    
     return 0;
 }
