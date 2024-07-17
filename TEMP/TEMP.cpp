2ea852b7be6117627465377a987d7080cf50e573
ChandraPrakash24 <2412prakas@gmail.com>
2024-07-17 11:29:11 +0530

    added dsa questions

--- TEMP/TEMP.cpp
+++ TEMP/TEMP.cpp
@@ -1,31 +1,56 @@
-#include<iostream>
-#include<bits/stdc++.h>
+//{ Driver Code Starts
+#include <bits/stdc++.h>
 using namespace std;
 
-void permute(string str, int i){
+// } Driver Code Ends
 
-    if(i == str.length()-1){
-        cout<<str<<endl;
-        return;
+class Solution{
+    public:
+    //You need to complete this fucntion
+    
+    long long power(int N,int R)
+    {
+       
+        
     }
 
-    for(int k = i; k<str.length();k++){
-        swap(str[k],str[i]);
-        permute(str,i+1);
-        swap(str[k],str[i]);
+};
+
+//{ Driver Code Starts.
+
+// compute reverse of a number 
+long long rev(long long n)
+{
+    long long rev_num = 0;
+     while(n > 0) 
+      { 
+        rev_num = rev_num*10 + n%10; 
+        n = n/10; 
+      } 
+      return rev_num;
+}
+
+
+
+
+int main()
+{
+    int T;
+    cin>>T;//testcases
+    
+    while(T--)
+    {
+        long long N;
+        cin>>N;//input N
+        
+        long long R = 0; 
+        
+        // reverse the given number n
+        R = rev(N);
+        Solution ob;
+        //power of the number to it's reverse
+        long long ans =ob.power(N,R);
+        cout << ans<<endl;
     }
-
 }
-
-int main(){
-    // string str = "AB"; // AB BA
-    string str = "ABC";
-    permute(str, 0);
-}
-//out:-
-// ABC
-// ACB
-// BAC
-// BCA
-// CBA
-// CAB
+// } Driver Code Ends
