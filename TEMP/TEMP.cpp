50c29882bcb9687889b4a3340a70fd180e973f27
ChandraPrakash24 <2412prakas@gmail.com>
2024-07-21 12:48:03 +0530

    added dsa questions

--- TEMP/TEMP.cpp
+++ TEMP/TEMP.cpp
@@ -1,72 +1,32 @@
-// all possible words from keypad
-// TC: O(4^n * n)   SC: O(n)
+// maximum length even odd
+// TC: O(n)   SC: O(1)
+// Kaden's algoritham
 
 #include<bits/stdc++.h>
 using namespace std;
 
-void possibleWordsHelper(int* a, int N,int index, vector<string>& keypad, vector<string>& res, string op){
-    if(index == N){
-        res.push_back(op);
-        return;
+int maximumLengthEvenOdd(int* a, int n){
+
+    int curr = 1, res = 1; // including first element
+    
+    for(int i=1;i<n;i++){
+        if((a[i] % 2 == 0 && a[i-1] % 2 != 0) || (a[i] % 2 != 0 && a[i-1] % 2 == 0)){
+            curr++;
+            res = max(curr,res);
+        }else{
+            curr = 1;
+        }
     }
-
-    string key = keypad[a[index]];
-
-    for(int i=0;i<key.length();i++){
-        string op1 = op;
-        possibleWordsHelper(a,N,index+1,keypad,res,op1+=key[i]);
-    }
-
-    return;
-
-}
-
-vector<string> possibleWords(int a[], int N){
-    vector<string> res;
-    vector<string> keypad = {"","","abc","def","ghi", "jkl","mno","pqrs","tuv","wxyz"};
-
-    int index=0;
-
-    possibleWordsHelper(a,N,index,keypad,res,string(""));
-
     return res;
-
 }
+
 
 int main()
 {
-    int N; cin >> N; 
-    
-    int a[N]; 
-    
-    for(int i =0;i<N;i++){
-        cin >> a[i]; 
-    }
-    
-    vector <string> res = possibleWords(a,N);
-    for (string i : res) cout << i << " ";
-    cout << endl;
+    int arr[] = {5,10,20,6,3,8};
+    int n = 6;
+
+    cout<<maximumLengthEvenOdd(arr,n); // 3
 
     return 0;
 }
-
-/*
-3
-5 8 9
-jtw jtx jty jtz juw jux juy 
-juz jvw jvx jvy jvz ktw ktx 
-kty ktz kuw kux kuy kuz kvw 
-kvx kvy kvz ltw ltx lty ltz 
-luw lux luy luz lvw lvx lvy 
-lvz
-
-3
-7 8 9
-ptw ptx pty ptz puw pux puy 
-puz pvw pvx pvy pvz qtw qtx 
-qty qtz quw qux quy quz qvw 
-qvx qvy qvz rtw rtx rty rtz 
-ruw rux ruy ruz rvw rvx rvy 
-rvz stw stx sty stz suw sux 
-suy suz svw svx svy svz
- */
