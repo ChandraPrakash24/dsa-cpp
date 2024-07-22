f2f10d6abfeb48e5b3834c6f51a9bb0b602b2498
ChandraPrakash24 <2412prakas@gmail.com>
2024-07-22 11:44:53 +0530

    added dsa questions

--- TEMP/TEMP.cpp
+++ TEMP/TEMP.cpp
@@ -1,32 +1,43 @@
-// maximum length even odd
-// TC: O(n)   SC: O(1)
-// Kaden's algoritham
-
 #include<bits/stdc++.h>
 using namespace std;
 
-int maximumLengthEvenOdd(int* a, int n){
-
-    int curr = 1, res = 1; // including first element
-    
-    for(int i=1;i<n;i++){
-        if((a[i] % 2 == 0 && a[i-1] % 2 != 0) || (a[i] % 2 != 0 && a[i-1] % 2 == 0)){
-            curr++;
-            res = max(curr,res);
-        }else{
-            curr = 1;
-        }
-    }
-    return res;
-}
-
-
 int main()
 {
-    int arr[] = {5,10,20,6,3,8};
+    int arr[] = { 10, 2, 3, 99, 12, 0 };
     int n = 6;
 
-    cout<<maximumLengthEvenOdd(arr,n); // 3
-
+    for(int i=0;i<n;i++){
+        for(int j=i; j<n;j++){
+            for(int k=i; k<=j; k++){
+                cout<<arr[k]<<" ";
+            }
+            cout<<endl;
+        }
+    } 
+    
     return 0;
 }
+
+/*
+10
+10 2
+10 2 3
+10 2 3 99
+10 2 3 99 12
+10 2 3 99 12 0
+2
+2 3
+2 3 99
+2 3 99 12
+2 3 99 12 0
+3
+3 99
+3 99 12
+3 99 12 0
+99
+99 12
+99 12 0
+12
+12 0
+0
+ */
