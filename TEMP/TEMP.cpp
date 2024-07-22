aa643b97ec2c60628cfc8623332c411da05d1be8
ChandraPrakash24 <2412prakas@gmail.com>
2024-07-22 14:28:53 +0530

    added dsa questions

--- TEMP/TEMP.cpp
+++ TEMP/TEMP.cpp
@@ -1,43 +1,30 @@
+// maximum sub-array sum (optmize solution)
+// TC: O(n)   SC: O(1)
+// KADANE'S ALGORITHAM
+
 #include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    int arr[] = { 10, 2, 3, 99, 12, 0 };
-    int n = 6;
+    int arr[] = {-2,-3,4,-1,-2,1,5,-3}; // 7
+    // int arr[] = {2}; // 2
+    // int arr[] = {5,4,1,7,8}; // 25
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-    for(int i=0;i<n;i++){
-        for(int j=i; j<n;j++){
-            for(int k=i; k<=j; k++){
-                cout<<arr[k]<<" ";
-            }
-            cout<<endl;
+    int max_sum = arr[0];
+    int curr_sum = arr[0];
+
+    for(int i=1; i<n; i++){
+        if((arr[i] + curr_sum) >= 0){
+            curr_sum += arr[i];
+            max_sum = max(curr_sum,max_sum);
+        }else{
+            curr_sum = 0;
         }
-    } 
+    }
     
+    cout<<max_sum;
+
     return 0;
 }
-
-/*
-10
-10 2
-10 2 3
-10 2 3 99
-10 2 3 99 12
-10 2 3 99 12 0
-2
-2 3
-2 3 99
-2 3 99 12
-2 3 99 12 0
-3
-3 99
-3 99 12
-3 99 12 0
-99
-99 12
-99 12 0
-12
-12 0
-0
- */
