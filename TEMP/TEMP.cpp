6710e843d793794db86ddd49acdd28f17416655b
ChandraPrakash24 <2412prakas@gmail.com>
2024-08-12 12:05:35 +0530

    added dsa questions

--- TEMP/TEMP.cpp
+++ TEMP/TEMP.cpp
@@ -1,43 +1,56 @@
-// Variable size sliding window
-// TC : O (n)   SC : O (1)
+this code is correct producing correct out put but i dot get the logic of "getSum(1, 1, res)":-
+please dry run for this. (under the hood)
+
+
+code:-
 
 #include<bits/stdc++.h>
 using namespace std;
 
-bool subarrWithSum(vector<int>& arr, int sum) {
+vector<int> getPrefix(vector<int>& arr, int size) {
+    vector<int> v(size, 0);
 
-    int res = 0, start = 0, end = 0;
+    int prefix = 0;
 
-    while (start <= end && end < arr.size()) {
-        if (res <= sum) {
-            res += arr[end++];
-            cout << res << " ";
-            if (res == sum) {
-                cout << start << " <-> " << end - 1 << endl;
-                return true;
-            }
-        }
-        else {
-            res -= arr[start++];
-            if (res == sum) {
-                cout << start << " <-> " << end - 1 << endl;
-                return true;
-            }
-        }
+    for (int i = 0; i < size;i++) {
+        v[i] = prefix + arr[i];
+        prefix = v[i];
     }
 
-    return false;
+    return v;
+}
 
+int getSum(int l, int r, vector<int>& sum) {
+    return l == 0 ? sum[r] : sum[r] - sum[l - 1];
 }
 
 int main()
 {
-    vector<int> v(6);
-    v = { 1,4,20,3,10,5 };
+    int n; cin >> n;
+    vector<int> arr(n);
+    for (auto& i : arr) cin >> i;
 
-    if (subarrWithSum(v, 26)) cout << "Yes" << endl;
-    else cout << "No" << endl;
+    vector<int> res = getPrefix(arr, n);
+
+    for (auto& i : res) cout << i << " ";
+
+    cout << getSum(0, 0, res) << endl;
+    cout << getSum(0, 1, res) << endl;
+    cout << getSum(1, 1, res) << endl;
+    cout << getSum(1, 3, res) << endl;
+    cout << getSum(3, 6, res) << endl;
 
 
     return 0;
 }
+
+// OUTPUT:-
+
+// 7
+// 1 2 3 4 5 6 7
+// 1 3 6 10 15 21 28
+// 1
+// 3
+// 2
+// 9
+// 22
