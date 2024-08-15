0d9858a8c2fc1c51d1b307b73ea8881bb623dc31
ChandraPrakash24 <2412prakas@gmail.com>
2024-08-15 14:23:10 +0530

    added dsa questions

--- TEMP/TEMP.cpp
+++ TEMP/TEMP.cpp
@@ -1,35 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool isThreePartision(vector<int>& arr, int n) {
-    int sum = 0;
-    for (auto& i : arr) sum += i;
+int  MaxAppEl(int* left, int l, int* right, int r) {
 
-    if ((sum % 3) != 0) return false; // if sum is not divisible by 3 then return false
+    vector<int> freq(100, 0);
+    int lI = 0, rI = 0;
 
-    int tragetSum = sum / 3;
-    int currentSum = 0;
-    int partisionCount = 0;
-
-    for (int i = 0; i < n; i++) {
-        currentSum += arr[i];
-        if (currentSum == tragetSum) {
-            partisionCount++;
-            currentSum = 0;
-        }
-        if (partisionCount == 2) return true;
+    while (!(lI >= l && rI >= r)) {
+        if (lI < l) freq[left[lI]] = 1;
+        if (rI < r) freq[right[rI]] = -1;
+        lI++;
+        rI++;
     }
 
-    return false;
+    int res = 0, resIndex = freq[0] == 1 ? 1 : 0;
+    for (int i = 0; i < 100; i++) {
+        if (freq[i] == 1) freq[i] = ++res;
+        else if (freq[i] == -1) freq[i] = --res;
+        else freq[i] = res;
+
+        if (i != 0 && freq[i] > freq[i - 1]) resIndex = i;
+    }
+
+    return resIndex;
 }
 
 int main()
 {
-    vector<int> arr(8);
-    // arr = { 5,2,6,1,1,1,1,4 }; // 1
-    arr = { 5,2,6,1,1,1,1,5 }; // 0
+    // int left[] = { 2,3,1 }; // 3 (3 and 4 both come 3 time but we return first occurance)
+    // int right[] = { 5,4,6 };
+    // // int left[] = { 1,2,0 }; // 2 (2 and 3 both come 3 time but we return first occurance)
+    // // int right[] = { 4,3,5 };
+    // // int left[] = { 1,2,4 };
+    // // int right[] = { 4,5,7 };
 
-    cout << isThreePartision(arr, 8);
+    // cout << "Maximum apparing elemnt in the given range array is: " << MaxAppEl(left, 3, right, 3);
+
+
+    int left[] = { 10, 15, 30 };
+    int right[] = { 15, 25, 35 };
+
+    cout << MaxAppEl(left, 3, right, 3);
 
     return 0;
 }
