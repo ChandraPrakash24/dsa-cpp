523be060529e10cc155179742ecaca877b0a3862
ChandraPrakash24 <2412prakas@gmail.com>
2024-08-21 16:45:16 +0530

    added dsa questions

--- TEMP/TEMP.cpp
+++ TEMP/TEMP.cpp
@@ -1,50 +1,25 @@
-#include <iostream>
-#include <vector>
-#include <algorithm> // For std::sort
-#include <cmath> // For std::floor
-
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int N;
-    cin >> N;
-    
-    vector<int> a(N);
-    int sum = 0;
-    
-    for (int i = 0; i < N; ++i) {
-        cin >> a[i];
-        sum += a[i];
-    }
+int main()
+{
+    vector<int> arr = { 1, 2, 3, 4, 5, 6, 7 };
+    int k = 3;
+    int n = 7;
 
-    // Calculate mean and floor it
-    double mean = static_cast<double>(sum) / N;
-    int floored_mean = static_cast<int>(floor(mean));
-    
-    // Sort the array
-    sort(a.begin(), a.end());
-    
-    // Calculate median
-    int median;
-    if (N % 2 == 1) {
-        // Odd number of elements
-        median = a[N / 2];
-    } else {
-        // Even number of elements
-        int mid1 = a[N / 2 - 1];
-        int mid2 = a[N / 2];
-        median = static_cast<int>(floor((mid1 + mid2) / 2.0));
-    }
-    
-    // Output mean and median
-    cout << floored_mean << " " << median << endl;
-    
+    k = k % n;
+    cout << k << endl;
+
+    // rotate to Right
+    reverse(arr.begin(), arr.begin() + k);
+    reverse(arr.begin() + k, arr.end());
+    // rotate to left
+    // reverse(arr.begin(),arr.begin()+(n-k));
+    // reverse(arr.begin()+(n-k),arr.end());
+    reverse(arr.begin(), arr.end());
+
+
+    for (auto& i : arr) cout << i << " ";
+
     return 0;
 }
-
-/*
-    Input:
-    N = 4
-    a[] = {2, 8, 3, 4}
-    Output: 4 3
- */
