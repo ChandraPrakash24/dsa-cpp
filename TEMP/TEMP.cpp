bceb452fdbe44799e80bb659605371de7f2fbba6
ChandraPrakash24 <2412prakas@gmail.com>
2024-08-22 14:54:42 +0530

    added dsa questions

--- TEMP/TEMP.cpp
+++ TEMP/TEMP.cpp
@@ -1,26 +1,76 @@
-// Print all Leaders
-// TC: O(n) SC: O(1)
-
-#include<bits/stdc++.h>
+//{ Driver Code Starts
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int arr[] = {30, 10, 10, 5};
-    int n = 4;
 
-    int prevLeader = arr[n - 1];
-    int k = n - 2;
-    for (int i = n - 2;i >= 0; --i) {
-        if (arr[i] >= arr[i + 1] && arr[i] >= prevLeader) {
-            prevLeader = arr[i];
-            arr[k--] = arr[i];
+// } Driver Code Ends
+class Solution {
+public:
+
+    // Function to count the frequency of all elements from 1 to N in the array.
+    void frequencyCount(vector<int>& arr, int n, int p) {
+        // Define an offset that will be larger than any value in arr.
+
+        int i, j;
+
+        for (i = 0; j < n;) {
+            if (arr[i] > 0 && arr[i] <= n) {
+                j = arr[i] - 1;
+
+                if (arr[j] <= 0) {
+                    arr[i] = 0;
+                    arr[j]--;
+                }
+                else {
+                    swap(arr[j], arr[i]);
+                    arr[j] = -1;
+                }
+            }
+            else if (arr[i] > n) {
+                arr[i++] = 0;
+            }
+            else {
+                i++;
+            }
         }
-    }
 
-    for(int j=k+1; j<n; j++){
-        cout<<arr[j]<<" ";
+        for (int i = 0; i < n; i++)
+            arr[i] = -arr[i];
     }
+};
 
+
+//{ Driver Code Starts.
+
+int main() {
+    long long t;
+
+    // testcases
+    cin >> t;
+
+    while (t--) {
+
+        int N, P;
+        // size of array
+        cin >> N;
+
+        vector<int> arr(N);
+
+        // adding elements to the vector
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
+        cin >> P;
+        Solution ob;
+        // calling frequncycount() function
+        ob.frequencyCount(arr, N, P);
+
+        // printing array elements
+        for (int i = 0; i < N; i++)
+            cout << arr[i] << " ";
+        cout << endl;
+    }
     return 0;
 }
+
+// } Driver Code Ends
